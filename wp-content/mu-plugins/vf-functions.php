e5acd502feccf5697bd103dead1f62a56b618ab5
Nicholas <nicholas@vlogfund.com>
2018-06-07 23:22:31 +0200

    fix mu plugin error and add wpmdb-preserve-wc-stripe

--- wp-content/mu-plugins/vf-functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-add_filter( 'wpmdb_preserved_options', function( $options ) {
-$options[] = 'woocommerce_stripe_settings';
-return $options;
-});
---
+++ wp-content/mu-plugins/wpmdb-preserve-wc-stripe.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Migrate DB Pro Tweak
+Plugin URI: http://github.com/deliciousbrains/wp-migrate-db-pro-tweaks
+Description: Preserve woocommerce_stripe_settings during WP Migrate DB Pro migrations
+Author: Delicious Brains
+Version: 1
+*/
+add_filter( 'wpmdb_preserved_options', function ( $options ) {
+	$options[] = 'woocommerce_stripe_settings';
+	return $options;
+} );
