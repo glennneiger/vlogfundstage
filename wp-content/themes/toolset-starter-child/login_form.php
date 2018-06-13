32565c38ec8bac28d5621dbbb1f42fd34c3b0b2d
vf-nicholas <nicholas@vlogfund.com>
2018-06-13 08:36:59 +0200

    still fixing minor autfocus mobile bug

--- wp-content/themes/toolset-starter-child/login_form.php
+++ wp-content/themes/toolset-starter-child/login_form.php
@@ -1,8 +1,8 @@
 <?php // Template for my form shortcode ?>
     <form id="login_user" class="ajax-auth" action="login" method="post">
         <p class="status"></p>
+        <input type="text" autofocus="autofocus" style="display:none" />
         <input id="username" type="text" class="sf-popup-input required" placeholder="Username or Email" name="username" autocomplete='email' required>
         <input id="password" type="password" class="sf-popup-input required" placeholder="Enter Password" name="password" current-password required>
         <input class="frm_final_submit submit_button" type="submit" value="LOGIN" name="submit">
-        <input type="hidden" autofocus="true" />
     </form>
--- wp-content/themes/toolset-starter-child/registration_form.php
+++ wp-content/themes/toolset-starter-child/registration_form.php
@@ -1,9 +1,9 @@
 <form id="register_user" class="ajax-auth"  action="register" method="post">
     <p class="status"></p>
+    <input type="text" autofocus="autofocus" style="display:none" />
     <input id="signonname" type="text" name="signonname" placeholder="Username" class="sf-popup-input required" required>
     <input id="email" type="text" class="sf-popup-input required email" placeholder="Email" name="email" autocomplete='email' required>
     <input id="signonpassword" type="password" class="sf-popup-input required" placeholder="Password" name="signonpassword" new-password required>
     <input type="password" id="password2" class="sf-popup-input required" placeholder="Repeat Password" name="password2" new-password required>
     <input class="frm_final_submit submit_button" type="submit" value="SIGNUP">
-    <input type="hidden" autofocus="true" />
 </form>
