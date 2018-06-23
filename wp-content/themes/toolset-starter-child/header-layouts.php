762e9761fdd1d32ddc10fd89d62d6c0f0c23a349
vf-nicholas <nicholas@vlogfund.com>
2018-06-23 04:15:44 +0200

    fix author login bug +add ajax form submission

--- wp-content/themes/toolset-starter-child/header-layouts.php
+++ wp-content/themes/toolset-starter-child/header-layouts.php
@@ -10,7 +10,7 @@ remove_theme_mod( 'menu_floating' );
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link rel="icon" href="https://www.vlogfund.com/wp-content/uploads/2018/05/vf-favicon.png" type="image/x-icon" />
+	<link rel="icon" href="https://www.vlogfund.com/wp-content/uploads/2018/06/v_logo_icon.ico" type="image/x-icon" />
   <link rel="shortcut icon" href="https://www.vlogfund.com/wp-content/uploads/2018/05/vf-favicon.png" type="image/x-icon" />
 	<?php
 		do_action( 'wpbootstrap_before_wp_head' );
