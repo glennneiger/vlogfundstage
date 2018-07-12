b2b133d800226a92df0dc246a9af402ba973fddf
vf-nicholas <nicholas@vlogfund.com>
2018-07-12 11:32:46 +0200

    login and register form validation improvements

--- wp-content/themes/toolset-starter-child/js.js
+++ wp-content/themes/toolset-starter-child/js.js
@@ -1609,8 +1609,8 @@ jQuery(".page-checkout .country_to_state.country_select ").select2({ minimumResu
     if (!$(this).valid()) return false;
     $('p.status', this).show().text(ajax_auth_object.loadingmessage);
     action = 'ajaxlogin';
-    username = $('form#login_user #username').val();
-    password = $('form#login_user #password').val();
+    username = $('form#login_user #username_').val();
+    password = $('form#login_user #password_').val();
     email = '';
     //security = $('form#login #security').val();
     if ($(this).attr('id') == 'register_user') {
@@ -1679,6 +1679,28 @@ jQuery(".page-checkout .country_to_state.country_select ").select2({ minimumResu
     });
   else if (jQuery("#login_user").length)
     jQuery("#login_user").validate();
+
+
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "This field is required.",
+        remote: "Please fix this field.",
+        email: "Please enter a valid email address.",
+        url: "Please enter a valid URL.",
+        date: "Please enter a valid date.",
+        dateISO: "Please enter a valid date (ISO).",
+        number: "Please enter a valid number.",
+        digits: "Please enter only digits.",
+        creditcard: "Please enter a valid credit card number.",
+        equalTo: "Please enter the same password again.",
+        accept: "Please enter a value with a valid extension.",
+        maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+        minlength: jQuery.validator.format("Please enter at least {0} characters."),
+        rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+        range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+        max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+        min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+    });
 
 
 
--- wp-content/themes/toolset-starter-child/login_form.php
+++ wp-content/themes/toolset-starter-child/login_form.php
@@ -1,7 +1,7 @@
 <?php // Template for my form shortcode ?>
     <form id="login_user" class="ajax-auth" action="login" method="post">
         <p class="status"></p>
-        <input id="username" type="text" class="sf-popup-input required" placeholder="Username or Email" name="username" autocomplete='email' required>
-        <input id="password" type="password" class="sf-popup-input required" placeholder="Enter Password" name="password" current-password required>
+        <input id="username_" type="text" class="sf-popup-input required" placeholder="Username or Email" name="username" autocomplete='email' required>
+        <input id="password_" type="password" class="sf-popup-input required" placeholder="Enter Password" name="password" current-password required>
         <input class="frm_final_submit submit_button" type="submit" value="LOGIN" name="submit">
     </form>
