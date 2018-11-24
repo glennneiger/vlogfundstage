b71f7b4f1aca19a8453f8ed5f24860769d9066f7
vf-nicholas <nicholas@vlogfund.com>
2018-11-24 23:47:14 +0100

    fix syntax error

--- wp-content/themes/toolset-starter-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/toolset-starter-child/woocommerce/checkout/form-checkout.php
@@ -18,18 +18,16 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-f
+
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
-// If checkout registration is disabled and not logged in, the user cannot checkout
+// If checkout registration is disabled and not logged in, the user cannot checkout.
 if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
-	echo apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) );
+	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
 	return;
 }
 
 ?>
-
-
 
 
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
@@ -92,7 +90,7 @@ else {
                             <div class="sfc-checkout-billing-input-fields">
 
 
-	<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
+	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
