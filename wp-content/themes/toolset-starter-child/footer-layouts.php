1a6970cc66d2af19cda4224396563ab4155c177c
vf-nicholas <nicholas@vlogfund.com>
2019-02-16 15:04:34 +0100

    add instant.page script

---
+++ wp-content/themes/toolset-starter-child/footer-layouts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying the footer with Layouts
+ *
+ */
+
+	do_action( 'wpbootstrap_before_footer' );
+	do_action( 'wpbootstrap_after_footer' );
+	do_action( 'wpbootstrap_before_wp_footer' );
+	wp_footer();
+	do_action( 'wpbootstrap_after_wp_footer' );
+	?>
+  <script src="//instant.page/1.1.0" type="module" integrity="sha384-EwBObn5QAxP8f09iemwAJljc+sU+eUXeL9vSBw1eNmVarwhKk2F9vBEpaN9rsrtp"></script>
+	</body>
+</html>
