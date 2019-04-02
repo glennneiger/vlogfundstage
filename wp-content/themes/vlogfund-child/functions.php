597469ca9e247cc563dc55ad25d0ba6cdadf23da
vf-nicholas <nicholas@vlogfund.com>
2019-04-02 20:08:47 +0200

    disable ajax call from wc + checkout page style changes

--- wp-content/themes/vlogfund-child/functions.php
+++ wp-content/themes/vlogfund-child/functions.php
@@ -493,6 +493,13 @@ add_action( 'wp_enqueue_scripts', 'grd_woocommerce_script_cleaner', 99 );
 
 
 
+
+/** Disable Ajax Call from WooCommerce on front page and posts*/
+add_action( 'wp_enqueue_scripts', 'dequeue_woocommerce_cart_fragments', 11);
+function dequeue_woocommerce_cart_fragments() {
+if (is_front_page() || is_single() ) wp_dequeue_script('wc-cart-fragments');
+}
+
 //remove other wp related things
 
 
