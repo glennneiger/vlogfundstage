fd6daa0eba2e036c3ac4a6fa611d42036cff7428
krunalprajapati41 <krunalprajapati41@gmail.com>
2019-04-04 18:57:48 +0530

    Email Template Grey Text & Terms and Condition Link Update

--- wp-content/themes/vlogfund-child/inc/emails/campaign-status/draft.php
+++ wp-content/themes/vlogfund-child/inc/emails/campaign-status/draft.php
@@ -664,22 +664,6 @@
 														</tr>
 													</tbody>
 												</table>
-												<table class="row" style="border-collapse:collapse;border-spacing:0;display:table;padding:0;position:relative;text-align:left;vertical-align:top;width:100%">
-													<tbody>
-														<tr style="padding:0;text-align:left;vertical-align:top">
-															<th class="small-12 large-12 columns first last" style="Margin:0 auto;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0 auto;padding:0;padding-bottom:16px;padding-left:16px;padding-right:16px;text-align:left;width:564px">
-																<table style="border-collapse:collapse;border-spacing:0;padding:0;text-align:left;vertical-align:top;width:100%">
-																	<tr style="padding:0;text-align:left;vertical-align:top">
-																		<th style="Margin:0;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;padding:0;text-align:left">
-																			<center data-parsed="" style="min-width:532px;width:100%"><small align="center" class="float-center" style="color:#cacaca;font-size:80%">Thank you for creating a campaign on Vlogfund.<br>Your campaign is currently saved as a draft. If you wish to edit it log into Vlogfund<br>1.Click on Account, located on the right site of the navigation bar<br>2.Click ”My Campaigns” in the drop-down menu<br>3.Click on the little pencil to edit it</small></center>
-																		</th>
-																		<th class="expander" style="Margin:0;color:#0a0a0a;font-family:Helvetica,Arial,sans-serif;font-size:16px;font-weight:400;line-height:1.3;margin:0;padding:0!important;text-align:left;visibility:hidden;width:0"></th>
-																	</tr>
-																</table>
-															</th>
-														</tr>
-													</tbody>
-												</table>
 												<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;">
 													<tbody class="mcnTextBlockOuter">
 														<tr>
@@ -921,7 +905,7 @@
 																	<tbody>
 																		<tr>										
 																			<td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
-																				<em>Copyright © Vlogfund <?php echo date('Y');?>, All rights reserved.</em> | <a href="https://www.vlogfund.com/terms?utm_source=autoresponder&utm_medium=email&utm_campaign=campaign-creation&utm_content=textlink" target="_blank" style="Margin:0;color:#dc2c5d;font-family:Helvetica,Arial,sans-serif;font-weight:400;line-height:1.3;margin:0;padding:0;text-align:left;text-decoration:none">Terms & Conditions</a><br>
+																				<em>Copyright © Vlogfund <?php echo date('Y');?>, All rights reserved. | <a href="https://www.vlogfund.com/terms?utm_source=autoresponder&utm_medium=email&utm_campaign=campaign-creation&utm_content=textlink" target="_blank">Terms & Conditions</a></em><br>
 												 							</td>
 																		</tr>
 																	</tbody>
