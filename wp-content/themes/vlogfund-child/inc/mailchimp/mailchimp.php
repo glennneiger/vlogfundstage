609fb1f6e36d574ee7d3724f92b65a4efbbeb573
vf-nicholas <nicholas@vlogfund.com>
2019-04-23 20:56:42 +0200

    Revert "Mailchimp File Ignore Stuff"
    This reverts commit 81d7c9bf2987179185d5f859f8dfa5898b06097c.

--- wp-content/themes/vlogfund-child/inc/mailchimp/mailchimp.php
+++ wp-content/themes/vlogfund-child/inc/mailchimp/mailchimp.php
@@ -8,8 +8,6 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
  * @author Drew McLellan <drew.mclellan@gmail.com>
  * @version 2.2
  */ 
- 
- 
 if( !defined('VLOG_MAILCHIMP_LIST') ) :
 	define('VLOG_MAILCHIMP_LIST', '18f30240e9');
 endif;
