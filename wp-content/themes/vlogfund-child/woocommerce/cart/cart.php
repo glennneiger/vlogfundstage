33b3c75d61f22b7b30714053ac91ff5257f32229
krunalprajapati41 <krunalprajapati41@gmail.com>
2019-05-04 14:24:19 +0530

    Update Minimum Donation

--- wp-content/themes/vlogfund-child/woocommerce/cart/cart.php
+++ wp-content/themes/vlogfund-child/woocommerce/cart/cart.php
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-do_action( 'woocommerce_before_cart' ); ?>
+do_action( 'woocommerce_before_cart' );  ?>
 
 <form class="woocommerce-cart-form sfc-checkout-billing-container" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
